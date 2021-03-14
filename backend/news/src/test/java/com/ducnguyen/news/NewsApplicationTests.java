0f8bb96feafd2977d98a91ffaf6a93659947a9d2
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-03-14 14:13:55 -0700

    First commit

---
+++ backend/news/src/test/java/com/ducnguyen/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducnguyen.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
