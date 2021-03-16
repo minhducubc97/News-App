8d2d59ec0c32cc6b059f2bd2e9534f6da21a0952
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-03-16 11:40:15 -0700

    Connect articles frontend with backend

--- frontend/src/components/ArticleList.jsx
+++ frontend/src/components/ArticleList.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faList } from "@fortawesome/free-solid-svg-icons";
+import { faList, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 class ArticleList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getArticles();
+  }
+
+  getArticles = () => {
+    axios
+      .get("http://localhost:8080/api/v1/articles")
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ articles: data });
+      });
+  };
+
   render() {
     return (
       <div className="card border border-dark bg-dark text-white">
@@ -20,9 +41,32 @@ class ArticleList extends Component {
               </tr>
             </thead>
             <tbody>
-              <tr align="center">
-                <td colSpan="6">No articles found!</td>
-              </tr>
+              {this.state.articles.length === 0 ? (
+                <tr align="center">
+                  <td colSpan="6">No articles found!</td>
+                </tr>
+              ) : (
+                this.state.articles.map((article) => (
+                  <tr key={article.id}>
+                    <td>
+                      <img src={article.coverPhotoURL} width="25" height="25" />
+                      &nbsp; &nbsp;
+                      {article.title}
+                    </td>
+                    <td>{article.author}</td>
+                    <td>
+                      <div className="btn-group" role="group">
+                        <button className="btn btn-outline-primary btn-sm">
+                          <FontAwesomeIcon icon={faEdit} />
+                        </button>
+                        <button className="btn btn-outline-danger btn-sm">
+                          <FontAwesomeIcon icon={faTrash} />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </table>
         </div>
